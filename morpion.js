7eb091d2ca522bef50ee6c2dc2757475e45737f6
FabienSaliba <72040422+FabienSaliba@users.noreply.github.com>
2023-01-20 15:42:53 +0100

    fix meteo et ajout produit

--- morpion.js
+++
@@ -1,1 +0,0 @@
-var apiKey = "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"
---
+++ produit.js
@@ -0,0 +1,27 @@
+const input = document.querySelector("#codeBarre");
+input.addEventListener("keydown", findProduct);
+var nom;
+var marque;
+var categories;
+
+function findProduct(event) {
+    const keyPressed = event.keyCode;
+    if (keyPressed == 13) {
+
+        fetch(`https://world.openfoodfacts.org/api/v0/product/${input.value}.json}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                },
+            })
+            .then(res => res.json())
+
+            .then(info => {
+                console.log(info)
+                nom = info["product"]["abbreviated_product_name_fr"];
+                marque = info["product"]["brands"]
+                categories = info["product"]["categories"]
+                document.querySelector("#resultProduit").innerHTML = `Le produit est: ${nom} de la marque ${marque}<br>Les catégories sont:<br>               ${categories}`
+            })
+    }
+}
